2bcaab90e068169f87cbec49f671b9776ac39353
Monicabrlpt <monicabrlpt@gmail.com>
2017-05-14 22:46:17 -0700

    Initial Commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl519/monvie21/model/SourcesResponse.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl519.monvie21.model;
+
+import java.util.List;
+
+/**
+ * Created by hyuam on 18/04/2017.
+ */
+
+public class SourcesResponse {
+    public String status;
+    public List<Result> results;
+
+}
